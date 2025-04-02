95d5fb72b53849a62fa4feb0fab4f4327659d50e
Ramon Rocha <ramonrocha1989@gmail.com>
2025-04-02 09:38:41 -0300

    create update players part 1

---
+++ src/players/players.controller.ts
@@ -0,0 +1,14 @@
+import { Controller, Post } from '@nestjs/common';
+
+@Controller('api/v1/players')
+export class PlayersController {    
+    
+    @Post()
+    async createUpdateJogador(){
+        return JSON.stringify({
+            "nome": "Ramon"
+        })
+
+    }
+
+}
